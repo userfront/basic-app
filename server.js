aff86a3b65a8b11758e43e4f4697e48ef38fccb5
Tyler <tyler@anymod.com>
2020-03-03 11:03:13 -0800

    Add landing page

--- server.js
+++ server.js
@@ -24,20 +24,28 @@ function getPage(pageName) {
   });
 }
 
-// Set projectId from subdomain
+// Set local variables
 app.use(function(req, res, next) {
-  app.locals.projectId = req.subdomains.length
-    ? req.subdomains[req.subdomains.length - 1]
-    : "";
+  app.locals.isLocal = req.hostname.indexOf("userfront.dev") < 0;
+  app.locals.projectId = req.subdomains[0] || "";
   next();
 });
 
 app.get("/", (req, res) => {
-  fs.readFile("./pages/index.html", "utf8", (err, data) => {
-    if (err) {
-      return console.log(err);
-    }
+  // If the server is running locally or has a project ID,
+  // serve the landing page. Otherwise serve the index page.
+  let fileToSend;
+  if (app.locals.isLocal || app.locals.projectId) {
+    fileToSend = "./pages/landing.html";
+  } else {
+    fileToSend = "./pages/index.html";
+  }
 
+  fs.readFile(fileToSend, "utf8", (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.send("Problem loading page");
+    }
     res.send(data);
   });
 });

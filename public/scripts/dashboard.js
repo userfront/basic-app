35bcf40f3638900965b11094d76e349d96915602
Tyler <tyler@anymod.com>
2021-04-01 16:10:21 -0700

    Update look of authorization

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -65,7 +65,7 @@ function showLoggedIn(user) {
   userId: ${token.userId},
   userUuid: "${token.userUuid}",
   isConfirmed: ${token.isConfirmed},
-  authorization: ${JSON.stringify(token.authorization, null, 2)},
+  authorization: ${JSON.stringify(token.authorization, null, 4)},
 }`;
   // Show card
   document.getElementById("logged-in").style.display = "block";
